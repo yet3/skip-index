e0e794500fdb3eb4d23f7d51a4aff95935c78ed3
Max Kasperowicz <yet3.dev@gmail.com>
2026-06-18 22:44:22 +0200

    fix: name of the noExternal shared-lib

--- packages/ts-plugin/tsup.config.ts
+++ packages/ts-plugin/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: ["src/index.ts"],
 	format: ["cjs"],
-	noExternal: ["@shared-lib"],
+	noExternal: ["shared-lib"],
 	external: ["vite"],
 	dts: true,
 	clean: true,
